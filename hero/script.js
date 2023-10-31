22074328d93bf5cb9be22a01f08414a235cc1510
Priyanshu Choudhury <priyanshu.choudhury2406@outlook.com>
2023-11-01 01:13:30 +0530

    animations added on navbar

--- hero/script.js
+++ hero/script.js
@@ -15,3 +15,32 @@ tl.from(".home-content p",{
     y: "50%",
     duration: 0.5
 });
+
+$(".navbar div").on("mouseover",(e)=>{
+    gsap.to(e.currentTarget.querySelector("a"),{
+        y: 0,
+        opacity: 1,
+        duration: 0.2,
+        ease: "none"
+    });
+    gsap.to(e.currentTarget.querySelector("i"),{
+        y: "-1vw",
+        opacity: 0,
+        duration: 0.2,
+        ease: "none"
+    });
+})
+$(".navbar div").on("mouseleave",(e)=>{
+    gsap.to(e.currentTarget.querySelector("a"),{
+        y: "1vw",
+        opacity: 0,
+        duration: 0.2,
+        ease: "none"
+    });
+    gsap.to(e.currentTarget.querySelector("i"),{
+        y: 0,
+        opacity: 1,
+        duration: 0.2,
+        ease: "none"
+    });
+})
